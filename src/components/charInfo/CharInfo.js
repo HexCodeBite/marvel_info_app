4ee9d54d7b634527a9fff3acdf3e3152111b2c2b
Vladimir <ursatiivladimir@gmail.com>
2025-01-24 21:49:54 +0300

    added prop check

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import PropTypes from 'prop-types';
 import './charInfo.scss';
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
@@ -106,4 +107,7 @@ const View = ({char}) => {
     )
 }
 
+CharInfo.propTypes = {
+    charId: PropTypes.number,
+}
 export default CharInfo;
